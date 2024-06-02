bc1ff9eacdb9db8057d3236c5958a26d8bd9e728
Vasilika Schnitzer <vasilika.schnitzer@gmail.com>
2024-06-02 19:38:29 +0200

    Feat: Add basic filter by user functionality

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('<slug:slug>/', views.entry_details, name='entry_details'),
+    path('filter/<str:username>/', views.filter_user, name='filter_user'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 from .models import Entry
 
@@ -10,6 +12,8 @@ from .models import Entry
 def index(request):
     entries = Entry.objects.all().filter(publish=1)
     sorted_param = ''
+    User = get_user_model()
+    users = User.objects.all()
 
     if request.GET.get('sorted') == 'by_likes':
         entries = Entry.objects.all().filter(publish=1).order_by('-likes')
@@ -27,5 +31,47 @@ def index(request):
         'mainpage/index.html',
         {'entries': entries,
          'sorted_param': sorted_param,
-         'page_obj': page_obj})
+         'page_obj': page_obj,
+         'users': users,
+         })
 
+
+def entry_details(request, slug):
+    entry = get_object_or_404(Entry.objects.filter(publish=1), slug=slug)
+    User = get_user_model()
+    users = User.objects.all()
+    
+    return render(
+        request,
+        'mainpage/entry_details.html',
+        {'entry': entry,
+         'users': users,
+         })
+
+
+def filter_user(request, username):
+    User = get_user_model()
+    users = User.objects.all()
+    user = get_object_or_404(users, username=username)
+    entries = user.entries.all().filter(publish=1)
+    sorted_param = ''
+    
+    if request.GET.get('sorted') == 'by_likes':
+        entries = entries.order_by('-likes')
+        sorted_param = '?sorted=by_likes'
+    elif request.GET.get('sorted') == 'by_date':
+        entries = entries.order_by('-created_on')
+        sorted_param = '?sorted=by_date'
+
+    paginator = Paginator(entries, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(
+        request,
+        'mainpage/index.html',
+        {'entries': entries,
+         'users': users,
+         'page_obj': page_obj,
+         'sorted_param': sorted_param,
+         })
