35765ec29289fa6f77fa230f77101d59788133c2
Vasilika Schnitzer <vasilika.schnitzer@gmail.com>
2024-06-02 17:20:01 +0200

    Fix: Change sort buttons method to GET

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-# from mainpage import index as main_index
 
 urlpatterns = [
     path('', views.index, name='home'),
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404, reverse
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Entry
 
 # Create your views here.
@@ -7,20 +7,15 @@ from .models import Entry
 
 def index(request):
     entries = Entry.objects.all().filter(publish=1)
+
+    if request.GET.get('sorted') == 'by_likes':
+        entries = Entry.objects.all().filter(publish=1).order_by('-likes')
+    elif request.GET.get('sorted') == 'by_date':
+        entries = Entry.objects.all().filter(publish=1).order_by('-created_on')
     
-    if request.method == "POST":
-        print("Posted")
-        if 'by_likes' in request.POST:
-            entries = Entry.objects.all().filter(publish=1).order_by('-likes')
-        elif 'by_date' in request.POST:
-            entries = Entry.objects.all().filter(publish=1).order_by('-created_on')
     
     return render(
         request,
         'mainpage/index.html',
         {'entries': entries})
 
-
-
-
-# For sort button: if 'sort_likes' in request.POST
