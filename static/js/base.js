f47aa4fc24ef279bb42e4fec2b4a293d8c481118
Vasilika Schnitzer <vasilika.schnitzer@gmail.com>
2024-06-23 16:19:15 +0200

    Fix: Fix sidebar toggle button behaviour, refactor js code

--- static/js/base.js
+++ static/js/base.js
@@ -1,78 +1,97 @@
+var sidebarCollapsed = true;
+var sidebarAutoCollapse = false;
+var sideBar = document.getElementById("sidebar");
+var sideBarState = getComputedStyle(sideBar).position;
+
 // Prevent filter items from collapsing once opened (on large screens)
 window.addEventListener("DOMContentLoaded", function () {
-  var sideBar = document.getElementById("sidebar");
-  var sideBarState = getComputedStyle(sideBar).position;
   var filterLinks = document.getElementsByClassName("filter-link");
-  var filterTitles = document.getElementsByClassName("filter-title");
   var filterCategories = document.getElementsByClassName("filter-cat");
+  var sideBarState = getComputedStyle(sideBar).position;
 
+  // Check whether the user is on a mobile device
   if (sideBarState === "absolute") {
-    var sidebarCollapsed = true;
+    sidebarAutoCollapse = true;
   } else {
-    var sidebarCollapsed = false;
+    sidebarAutoCollapse = false;
   }
 
-
-  //   alert(filterCategories);
+  // Keep the list of usernames or tags expanded if the user is not on a
+  // mobile device and if they already clicked on a filter and the view
+  // shows a filtered selection of entries
   for (let filterCat of filterCategories) {
     if (
-      sideBarState != "absolute" &&
-      filterCat.classList.contains("force-show") &&
-      !sidebarCollapsed
+      !sidebarAutoCollapse &&
+      filterCat.classList.contains("force-show")
     ) {
+      expandSidebar();
       filterCat.classList.add("show");
-    } else if (sidebarCollapsed) {
+    } else if (
+      !sidebarAutoCollapse &&
+      filterCat.classList.contains("force-show")
+    ) {
       filterCat.classList.remove("show");
     }
   }
+
   for (let link of filterLinks) {
     link.addEventListener("click", function () {
-      if (sideBarState === "absolute") {
-        var currentTitle = link.children[1];
-        // only do it if it is uncollapsed
-        if (link.getAttribute("aria-expanded") === "true") {
-          currentTitle.classList.remove("d-none");
-          currentTitle.classList.add("d-inline");
-        } else {
-          currentTitle.classList.add("d-none");
-          currentTitle.classList.remove("d-inline");
-        }
-        if (currentTitle.classList.contains("d-none")) {
-          for (title of filterTitles) {
-            title.classList.add("d-none");
-            title.classList.remove("d-inline");
-          }
-        } else {
-          for (title of filterTitles) {
-            title.classList.remove("d-none");
-            title.classList.add("d-inline");
-          }
-        }
-        // for (title of filterTitles) {
-        //   title.classList.toggle("d-none");
-        //   title.classList.toggle("d-md-none");
-        //   title.classList.toggle("d-inline");
-        //   title.classList.toggle("d-md-inline");
-        // }
-      } else {
-        if (!sidebarCollapsed && filterTitles[0].classList.contains("d-none")) {
-          for (title of filterTitles) {
-            title.classList.remove("d-none");
-            title.classList.add("d-inline");
-          }
-        } else if (!sidebarCollapsed) {
-          for (title of filterTitles) {
-            title.classList.add("d-none");
-            title.classList.remove("d-inline");
-            for (cat of filterCategories) {
-              cat.classList.remove("show");
-            }
-          }
-        }
+      // If the user clicks on an expandable item, no matter on what device, and
+      // the sidebar is collapsed, the sidebar will be expanded
+      if (link.getAttribute("aria-expanded") === "true" && sidebarCollapsed) {
+        expandSidebar();
       }
     });
   }
 });
+
+function collapseSidebar() {
+  // Toogle button icon
+  document.querySelector("#toggle-icon-expand").classList.add("d-inline");
+  document.querySelector("#toggle-icon-expand").classList.remove("d-none");
+  document.querySelector("#toggle-icon-collapse").classList.add("d-none");
+  document.querySelector("#toggle-icon-collapse").classList.remove("d-inline");
+
+  sidebarCollapsed = true;
+
+  // Hide the block with usernames or tags
+  var filterCategories = document.getElementsByClassName("filter-cat");
+  for (cat of filterCategories) {
+    cat.classList.remove("show");
+  }
+
+  // Hide all sidebar nav item titles
+  var filterTitles = document.getElementsByClassName("filter-title");
+  for (title of filterTitles) {
+    title.classList.add("d-none");
+    title.classList.remove("d-inline");
+  }
+
+  sideBar.style.minWidth = "initial";
+}
+
+function expandSidebar() {
+  // Toogle button icon
+  document.querySelector("#toggle-icon-expand").classList.remove("d-inline");
+  document.querySelector("#toggle-icon-expand").classList.add("d-none");
+  document.querySelector("#toggle-icon-collapse").classList.remove("d-none");
+  document.querySelector("#toggle-icon-collapse").classList.add("d-inline");
+
+  sidebarCollapsed = false;
+
+  // Show all sidebar nav item titles
+  var filterTitles = document.getElementsByClassName("filter-title");
+  for (title of filterTitles) {
+    title.classList.remove("d-none");
+    title.classList.add("d-inline");
+  }
+
+  if (sidebarAutoCollapse) {
+    sideBar.style.minWidth = "100%";
+  } else {
+    sideBar.style.minWidth = "17%";
+  }
+}
 
 // The menu toggle button in the navbar has a hover effect that stops working
 // once the button is clicked and therefore focused. The following code removes
@@ -87,34 +106,10 @@ document.getElementById("toggler-btn").addEventListener("click", function () {
 document
   .getElementById("sidebar-toggle-btn")
   .addEventListener("click", function () {
-    // Toggle the expand/collapse button icon
-    for (let element of this.children) {
-      element.classList.toggle("d-none");
-      element.classList.toggle("d-inline");
-    }
-
-    if (
-      document.querySelector("#toggle-icon-expand").classList.contains("d-none") // || d-md-inline
-    ) {
-      sidebarCollapsed = false;
+    if (sidebarCollapsed) {
+      expandSidebar();
     } else {
-      sidebarCollapsed = true;
-    }
-
-    var filterCategories = document.getElementsByClassName("filter-cat");
-    for (cat of filterCategories) {
-      cat.classList.remove("show");
-    }
-
-    var filterTitles = document.getElementsByClassName("filter-title");
-    for (title of filterTitles) {
-      if (!sidebarCollapsed) {
-        title.classList.remove("d-none");
-        title.classList.add("d-inline");
-      } else {
-        title.classList.add("d-none");
-        title.classList.remove("d-inline");
-      }
+      collapseSidebar();
     }
 
     // Use setTimeout to remove focus from the icon after clicking it
@@ -123,27 +118,15 @@ document
     }, 100);
   });
 
+window.addEventListener("resize", () => {
+  sideBarState = getComputedStyle(sideBar).position;
+  if (sideBarState === "absolute") {
+    sidebarAutoCollapse = true;
+  } else {
+    sidebarAutoCollapse = false;
+  }
+});
 
-  // window.addEventListener("resize", () => {
-  //   if (sidebarCollapsed === true) {
-  //     document.querySelector("#toggle-icon-expand").classList.remove("d-none");
-  //     document.querySelector("#toggle-icon-expand").classList.add("d-md-inline");
-  //     document.querySelector("#toggle-icon-collapse").classList.add("d-none");
-  //     document.querySelector("#toggle-icon-collapse").classList.remove("d-md-inline");
-  //   }
-  // });
-
-
-// Hide filter on small devices when sidebar is overlaying content
-// function hideFilter(item) {
-//   var w = window.innerWidth;
-//   var sideBar = document.getElementById("sidebar");
-//   var sideBarState = getComputedStyle(sideBar).position;
-//   var filterCategory = item.parentElement.parentElement;
-//   if (sideBarState === "absolute") {
-//     filterCategory.classList.remove("show");
-//   }
-// }
 
 const msgAlerts = document.querySelectorAll(".alert");
 
