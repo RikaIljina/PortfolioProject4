04f89b9006244386d44ddfae5c3a4371b4c85fe4
Vasilika Schnitzer <vasilika.schnitzer@gmail.com>
2024-06-26 18:45:28 +0200

    Deploy: Try to fix staticfile issue

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -1,80 +1,116 @@
-
-var sidebarCollapsed = false;
-
-
+var sidebarCollapsed = true;
+var sidebarAutoCollapse = false;
+const sideBar = document.getElementById("sidebar");
+var sideBarState = getComputedStyle(sideBar).position;
 
 // Prevent filter items from collapsing once opened (on large screens)
 window.addEventListener("DOMContentLoaded", function () {
-  var sideBar = document.getElementById("sidebar");
-  var sideBarState = getComputedStyle(sideBar).position;
   var filterLinks = document.getElementsByClassName("filter-link");
-  var filterTitles = document.getElementsByClassName("filter-title");
   var filterCategories = document.getElementsByClassName("filter-cat");
+  const sideBar = document.getElementById("sidebar");
+  var sideBarState = getComputedStyle(sideBar).position;
 
-  //   alert(filterCategories);
+  // Check whether the user is on a mobile device
+  if (sideBarState === "absolute") {
+    sidebarAutoCollapse = true;
+  } else {
+    sidebarAutoCollapse = false;
+  }
+
+  // Keep the list of usernames or tags expanded if the user is not on a
+  // mobile device and if they already clicked on a filter and the view
+  // shows a filtered selection of entries
   for (let filterCat of filterCategories) {
-    if (
-      sideBarState != "absolute" &&
-      filterCat.classList.contains("force-show") && !sidebarCollapsed
-    ) {
+    if (!sidebarAutoCollapse && filterCat.classList.contains("force-show")) {
+      expandSidebar();
       filterCat.classList.add("show");
-    } else if (sidebarCollapsed) {
+      break;
+    } else if (
+      !sidebarAutoCollapse &&
+      filterCat.classList.contains("force-show")
+    ) {
       filterCat.classList.remove("show");
     }
   }
+
   for (let link of filterLinks) {
     link.addEventListener("click", function () {
-      if (sideBarState === "absolute") {
-        var currentTitle = link.children[1];
-        // only do it if it is uncollapsed
-        if (link.getAttribute("aria-expanded") === "true") {
-          currentTitle.classList.remove("d-none");
-          currentTitle.classList.add("d-inline");
-        } else {
-          currentTitle.classList.add("d-none");
-          currentTitle.classList.remove("d-inline");
-        }
-        if (currentTitle.classList.contains("d-none")) {
-          for (title of filterTitles) {
-            title.classList.add("d-none");
-            title.classList.remove("d-inline");
-          }
-        } else {
-          for (title of filterTitles) {
-            title.classList.remove("d-none");
-            title.classList.add("d-inline");
-          }
-        }
-        // for (title of filterTitles) {
-        //   title.classList.toggle("d-none");
-        //   title.classList.toggle("d-md-none");
-        //   title.classList.toggle("d-inline");
-        //   title.classList.toggle("d-md-inline");
-        // }
-      } else {
-        if (sidebarCollapsed && filterTitles[0].classList.contains("d-none")) {
-          for (title of filterTitles) {
-            title.classList.remove("d-none");
-            title.classList.add("d-inline");
-            title.classList.remove("d-md-none");
-            title.classList.add("d-md-inline");
-          }
-        } else if (sidebarCollapsed) {
-          for (title of filterTitles) {
-            title.classList.add("d-none");
-            title.classList.remove("d-inline");
-            title.classList.add("d-md-none");
-            title.classList.remove("d-md-inline");
-            for (cat of filterCategories) {
-              cat.classList.remove("show");
-            }
-          }
-
-        }
+      // If the user clicks on an expandable item, no matter on what device, and
+      // the sidebar is collapsed, the sidebar will be expanded
+      if (link.getAttribute("aria-expanded") === "true" && sidebarCollapsed) {
+        expandSidebar();
       }
+      setTimeout(() => { 
+        document.querySelector("#about-link-container").style.width =
+        getComputedStyle(sideBar).width;
+      }, 200);
+      
     });
   }
+
+  document.querySelector("#about-link-container").style.width =
+    getComputedStyle(sideBar).width;
+
+  if (sideBarState === "absolute") {
+    // let newHeight = getComputedStyle(sideBar).height;
+    let newHeight = getComputedStyle(
+      document.querySelector(".main-content")
+    ).height; //style.minHeight; // = newHeight; //`calc(${getComputedStyle(sideBar).height} + 10rem);` //"calc(100vh + 10rem)";
+    document.querySelector("#sidebar").style.height = newHeight;
+  }
 });
+
+function collapseSidebar() {
+  // Toogle button icon
+  document.querySelector("#toggle-icon-expand").classList.add("d-inline");
+  document.querySelector("#toggle-icon-expand").classList.remove("d-none");
+  document.querySelector("#toggle-icon-collapse").classList.add("d-none");
+  document.querySelector("#toggle-icon-collapse").classList.remove("d-inline");
+
+  sidebarCollapsed = true;
+
+  // Hide the block with usernames or tags
+  var filterCategories = document.getElementsByClassName("filter-cat");
+  for (cat of filterCategories) {
+    cat.classList.remove("show");
+  }
+
+  // Hide all sidebar nav item titles
+  var filterTitles = document.getElementsByClassName("filter-title");
+  for (title of filterTitles) {
+    title.classList.add("d-none");
+    title.classList.remove("d-inline");
+  }
+
+  sideBar.style.minWidth = "initial";
+  document.querySelector("#about-link-container").style.width = "initial";
+}
+
+function expandSidebar() {
+  // Toogle button icon
+  document.querySelector("#toggle-icon-expand").classList.remove("d-inline");
+  document.querySelector("#toggle-icon-expand").classList.add("d-none");
+  document.querySelector("#toggle-icon-collapse").classList.remove("d-none");
+  document.querySelector("#toggle-icon-collapse").classList.add("d-inline");
+
+  sidebarCollapsed = false;
+
+  // Show all sidebar nav item titles
+  var filterTitles = document.getElementsByClassName("filter-title");
+  for (title of filterTitles) {
+    title.classList.remove("d-none");
+    title.classList.add("d-inline");
+  }
+
+  if (sidebarAutoCollapse) {
+    sideBar.style.minWidth = "100%";
+  } else {
+    sideBar.style.minWidth = "17%";
+  }
+alert('adjusting width')
+  document.querySelector("#about-link-container").style.width =
+    getComputedStyle(sideBar).width;
+}
 
 // The menu toggle button in the navbar has a hover effect that stops working
 // once the button is clicked and therefore focused. The following code removes
@@ -89,40 +125,10 @@ document.getElementById("toggler-btn").addEventListener("click", function () {
 document
   .getElementById("sidebar-toggle-btn")
   .addEventListener("click", function () {
-    // Toggle the expand/collapse button icon
-    for (let element of this.children) {
-      element.classList.toggle("d-none");
-      element.classList.toggle("d-md-none");
-      element.classList.toggle("d-inline");
-      element.classList.toggle("d-md-inline");
-    }
-
-    if (
-      document.querySelector("#toggle-icon-expand").classList.contains("d-none") // || d-md-inline
-    ) {
-      sidebarCollapsed = true;
+    if (sidebarCollapsed) {
+      expandSidebar();
     } else {
-      sidebarCollapsed = false;
-    }
-
-    var filterCategories = document.getElementsByClassName("filter-cat");
-    for (cat of filterCategories) {
-       cat.classList.remove("show");
-    }
-
-    var filterTitles = document.getElementsByClassName("filter-title");
-    for (title of filterTitles) {
-      if (!sidebarCollapsed) {
-        title.classList.remove("d-none");
-        title.classList.add("d-inline");
-        title.classList.remove("d-md-none");
-        title.classList.add("d-md-inline");
-      } else {
-        title.classList.add("d-none");
-        title.classList.remove("d-inline");
-        title.classList.add("d-md-none");
-        title.classList.remove("d-md-inline");
-      }
+      collapseSidebar();
     }
 
     // Use setTimeout to remove focus from the icon after clicking it
@@ -131,17 +137,39 @@ document
     }, 100);
   });
 
+window.addEventListener("resize", () => {
+  sideBarState = getComputedStyle(sideBar).position;
+  if (sideBarState === "absolute") {
+    sidebarAutoCollapse = true;
+  } else {
+    sidebarAutoCollapse = false;
+  }
 
-// Hide filter on small devices when sidebar is overlaying content
-// function hideFilter(item) {
-//   var w = window.innerWidth;
-//   var sideBar = document.getElementById("sidebar");
-//   var sideBarState = getComputedStyle(sideBar).position;
-//   var filterCategory = item.parentElement.parentElement;
-//   if (sideBarState === "absolute") {
-//     filterCategory.classList.remove("show");
-//   }
-// }
+  if (sideBarState === "absolute") {
+    // let newHeight = getComputedStyle(sideBar).height;
+    let newHeight = getComputedStyle(
+      document.querySelector(".main-content")
+    ).height; //style.minHeight; // = newHeight; //`calc(${getComputedStyle(sideBar).height} + 10rem);` //"calc(100vh + 10rem)";
+    document.querySelector("#sidebar").style.height = newHeight;
+  }
+  document.querySelector("#about-link-container").style.width =
+    getComputedStyle(sideBar).width;
+});
+
+
+
+window.addEventListener("scroll", function () {
+  const aboutLink = document.querySelector("#about-link-container");
+  const menu = document.querySelector("#menu");
+  const menuBottom = menu.getBoundingClientRect().bottom;
+  const aboutLinkTop = aboutLink.getBoundingClientRect().top;
+
+  if (aboutLinkTop < menuBottom) {
+    aboutLink.classList.add("overlay-shadow");
+  } else {
+    aboutLink.classList.remove("overlay-shadow");
+  }
+});
 
 const msgAlerts = document.querySelectorAll(".alert");
 
@@ -158,4 +186,3 @@ for (var activeTooltip of activeTooltips) {
   var tooltip = new bootstrap.Tooltip(activeTooltip, (placement = "top"));
   tooltip.show();
 }
-
--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -1,40 +1,54 @@
 // import 'bootstrap';
 // window.Modal = bootstrap.Modal;
+window.addEventListener("DOMContentLoaded", function () {
+  const editButtons = document.getElementsByClassName("btn-edit");
+  // const commentText = document.getElementById("id_content");
+  const commentForm = document.getElementById("commentForm");
 
-const editButtons = document.getElementsByClassName("btn-edit");
-// const commentText = document.getElementById("id_content");
-const commentForm = document.getElementById("commentForm");
+  // const deleteModal = new Modal(document.getElementById("deleteModal"));
+  const deleteButtons = document.getElementsByClassName("btn-delete");
+  const deleteConfirm = document.getElementById("deleteCommentConfirm");
 
-// const deleteModal = new Modal(document.getElementById("deleteModal"));
-const deleteButtons = document.getElementsByClassName('btn-delete');
-const deleteConfirm = document.getElementById('deleteConfirm');
-
-
-for (let button of editButtons) {
+  for (let button of editButtons) {
     button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("data-comment_id");
-        let commentParagraph = document.getElementById(`comment${commentId}`);
-        let commentContent = commentParagraph.innerText;
-        let hiddenFormDiv = document.getElementById(`commentform${commentId}`);
-        let editCommentForm = document.getElementById(`editCommentForm${commentId}`);
-        let inplaceCommentText = document.getElementById(`updatedComment${commentId}`);
-        hiddenFormDiv.removeAttribute('class', 'visually-hidden');
-        commentParagraph.setAttribute('class', 'visually-hidden');
-        // commentText.value = commentContent;
-        inplaceCommentText.value = commentContent;
-        //submitButton.innerText = "Update";
-        let oldPath = window.location.href.split('?')[0].slice(0, -1);
-       // commentForm.setAttribute("action", `${oldPath}/edit-comment/${commentId}/`);
-        editCommentForm.setAttribute("action", `${oldPath}/edit-comment/${commentId}/`);
-    });
-}
+      e.preventDefault();
+      let commentId = e.currentTarget.getAttribute("data-comment_id");
+      alert(commentId)
+      let commentParagraph = document.getElementById(`comment${commentId}`);
+      let commentContent = commentParagraph.innerText;
+      let hiddenFormDiv = document.getElementById(`commentform${commentId}`);
+      hiddenFormDiv.removeAttribute("class", "d-none");
 
-for (let button of deleteButtons) {
-    button.addEventListener("click", (e) => {
-        let commentId = e.target.getAttribute("data-comment_id");
-        let oldPath = window.location.href.split('?')[0].slice(0, -1);
-        //let (deleteConfirm.href);
-        deleteConfirm.href = `${oldPath}/delete-comment/${commentId}/`;
-        //deleteModal.show();
+      let editCommentForm = document.getElementById(
+        `editCommentForm${commentId}`
+      );
+      let inplaceCommentText = document.getElementById(
+        `updatedComment${commentId}`
+      );
+      //hiddenFormDiv.setAttribute("aria-hidden", "false");
+      commentParagraph.setAttribute("class", "visually-hidden");
+      // commentText.value = commentContent;
+      inplaceCommentText.value = commentContent;
+      //submitButton.innerText = "Update";
+      let oldPath = window.location.href.split("?")[0].slice(0, -1);
+      // commentForm.setAttribute("action", `${oldPath}/edit-comment/${commentId}/`);
+      editCommentForm.setAttribute(
+        "action",
+        `${oldPath}/edit-comment/${commentId}/`
+      );
     });
-}
+  }
+
+  for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      alert(e.currentTarget)
+      let commentId = e.currentTarget.getAttribute("data-comment_id");
+      let oldPath = window.location.href.split("?")[0].slice(0, -1);
+      //let (deleteConfirm.href);
+      alert(commentId);
+      deleteConfirm.href = `${oldPath}/delete-comment/${commentId}/`;
+      //deleteModal.show();
+    });
+  }
+});
